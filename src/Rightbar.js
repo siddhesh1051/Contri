83f52b5f95990d5ba2d809fd431fb18d653832a0
siddhesh1051 <siddhesh1052001@gmail.com>
2023-03-25 20:57:21 +0530

    shared file done

--- src/Rightbar.js
+++ src/Rightbar.js
@@ -4,10 +4,17 @@ import { db } from './firebase';
 import './css/Responsive.css'
 import toast, { Toaster } from 'react-hot-toast';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import ImageIcon from '@mui/icons-material/Image';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import { red, teal } from '@mui/material/colors';
 
 
 export const Rightbar = (props) => {
     const [user, setUser] = useState([])
+    const [message, setMessage] = useState([])
+
     const qr = query(collection(db, props.roomid));
 
 
@@ -18,6 +25,14 @@ export const Rightbar = (props) => {
             onSnapshot(qr, (snapshot) => setUser(snapshot.docs.map((doc) => [{ name: doc.data().name, img: doc.data().userimg }])))
         }
         , [props.roomid]);
+    useEffect(
+            () => {
+                onSnapshot(qr, (snapshot) => setMessage(snapshot.docs.map((doc) => doc.data())))
+            }
+            , [props.roomid]);
+
+            // console.log(message);
+
     user.forEach((item) => {
         map.set(item[0].name, item[0].img);
     })
@@ -31,7 +46,7 @@ export const Rightbar = (props) => {
             <Toaster />
 
 
-            <div className='leftbar' id='showleft' style={{ backgroundColor: '#252329', minWidth: '284px', position: 'relative', height: '100%', flex: '0.17', transition: 'all 0.25s',overflow:'scroll'}}>
+            <div className='leftbar' id='showleft' style={{ backgroundColor: '#252329', minWidth: '284px', position: 'relative', height: '100%', flex: '0.17', transition: 'all 0.25s',overflow:'auto'}}>
                 <div className="rommspecificidandp" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center',justifyContent:'center', marginTop: '30px',padding:'20px', backgroundColor:'#03336E', margin:'10px', borderRadius:'10px' }}>
                     <p style={{ color: 'white', fontFamily: 'cursive', fontWeight: '500', fontStyle: 'normal', fontSize: '32px', letterSpacing: '-0.035em', marginLeft: '31px', marginRight: '26px', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>{props.roomid}<ContentCopyIcon style={{ fontSize:'32px', marginLeft: '12px', width: '30px', marginBottom: '-1.8px', cursor: 'pointer', color: '#B9B6B6' }} onClick={() => {
                         navigator.clipboard.writeText(props.roomid); toast.success('Room id copied to clipboard', {
@@ -43,14 +58,15 @@ export const Rightbar = (props) => {
                     }} /></p>
 
                 </div>
+                
                { user&&user.length>0&&
-                <div className="sepater" style={{ overflowY: 'scroll', height: '60vh', padding:'5px', backgroundColor:'#2D2D2F', margin:'10px', borderRadius:'10px'  }}>
+                <div className="sepater" style={{ overflowY: 'auto', maxHeight: '300px', padding:'5px', backgroundColor:'#2D2D2F', margin:'10px', borderRadius:'10px'  }}>
                 <h5 className="memheader" style={{fontFamily:'cursive'}}>Members</h5>
                     
                     {
                         Array.from(map, ([key, value]) => {
                             return (
-                                <div className="people" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: '16px', marginBottom: '12px' }}>
+                                <div key={key} className="people" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: '16px', marginBottom: '12px' }}>
                                     {/* <FiberManualRecordIcon color="disabled" style={{ width: '10px', marginLeft: '29.99px', marginTop: '1px' }} /> */}
                                     <div className="imagebox" style={{ width: '36px', height: '36px', borderRadius: '100%', marginLeft: '20px' }}>
                                         <img src={value} style={{ width: '34px', height: '34px', borderRadius: '11px' }} alt="" />
@@ -61,8 +77,73 @@ export const Rightbar = (props) => {
                     }
                 </div>}
 
-                <div className="sepater" style={{maxHeight: '50vh', padding:'5px', backgroundColor:'#2D2D2F', margin:'10px', borderRadius:'10px'  }}>
-                <h5 className="memheader" style={{fontFamily:'cursive'}}>Shared Files&nbsp; : </h5>
+                <div className="sepater" style={{overflowY:'auto', maxHeight: '300px', padding:'5px', backgroundColor:'#2D2D2F', margin:'10px', borderRadius:'10px'  }}>
+                <h5 className="memheader" style={{fontFamily:'cursive' ,marginBottom:'15px',marginLeft:'12px'}}>Shared Files</h5>
+
+                    
+                {
+                    message&&message.length>0&&message.map((item)=>{
+                        {   
+                            //image
+                            if(item.filetype==="image/jpeg" || item.filetype==="image/png" || item.filetype==="image/jpg"){
+                                return (
+                                <a key={item.text} href={item.text} target="_blank" className="chat__body__message" rel="noreferrer" style={{ paddingTop: '0px', textDecoration: 'none', MarginRight: '9px', marginLeft: '10px', marginBottom: '5px', fontSize: '13px' }}>
+                                <div className='fileDiv' style={{display:'flex',flexDirection:'row', padding:'5px',backgroundColor:'#3C393F',borderRadius:'10px',width:'90%'}}>
+
+                                <ImageIcon style={{ marginRight: '7.5px', fontSize: '24px', color: 'violet' }}></ImageIcon> <span style={{ marginRight: '7.5px', fontSize: '13px'}} >{item.filename}</span>
+                                </div>
+                                 </a>
+                            
+                            )
+
+                            //pdf
+                            }else if(item.filetype==="application/pdf"){
+                                return (
+                                    <a key={item.text} href={item.text} target="_blank" className="chat__body__message" rel="noreferrer" style={{ paddingTop: '0px', textDecoration: 'none', MarginRight: '9px', marginLeft: '10px', marginBottom: '5px', fontSize: '13px' }}>
+                                <div className='fileDiv' style={{display:'flex',flexDirection:'row', padding:'5px',backgroundColor:'#3C393F',borderRadius:'10px',width:'90%'}}>
+        
+                                <PictureAsPdfIcon style={{ marginRight: '7.5px', fontSize: '24px', color: 'red' }}></PictureAsPdfIcon> <span style={{ marginRight: '7.5px', fontSize: '13px'}}>{item.filename}</span>
+                                </div>
+                                 </a>
+                            
+                            )
+
+                            //video
+                            }else if(item.filetype==="video/mp4"){
+                                return (
+                                    <a key={item.text} href={item.text} target="_blank" className="chat__body__message" rel="noreferrer" style={{ paddingTop: '0px', textDecoration: 'none', MarginRight: '9px', marginLeft: '10px', marginBottom: '5px', fontSize: '13px' }}>
+                                <div className='fileDiv' style={{display:'flex',flexDirection:'row', padding:'5px',backgroundColor:'#3C393F',borderRadius:'10px',width:'90%'}}>
+                            
+                                <VideocamIcon style={{ marginRight: '7.5px', fontSize: '24px', color: 'pink' }}></VideocamIcon> <span style={{ marginRight: '7.5px', fontSize: '13px'}}>{item.filename}</span>
+                                 </div>
+                                 </a>
+
+                            
+                            )
+
+                            //audio
+                            }else if(item.filetype==="audio/mpeg"){
+                                return (
+                                    
+                                    <a key={item.text} href={item.text} target="_blank" className="chat__body__message" rel="noreferrer" style={{ paddingTop: '0px', textDecoration: 'none', MarginRight: '9px', marginLeft: '10px', marginBottom: '5px', fontSize: '13px' }}>
+                                <div className='fileDiv' style={{display:'flex',flexDirection:'row', padding:'5px',backgroundColor:'#3C393F',borderRadius:'10px',width:'90%'}}>
+
+                                <LibraryMusicIcon style={{ marginRight: '7.5px', fontSize: '24px', color: 'teal' }}></LibraryMusicIcon> <span style={{ marginRight: '7.5px', fontSize: '13px'}}>{item.filename}</span>
+                                 </div>
+                                 </a>
+                            
+                            )
+
+
+
+
+                            }
+
+                                        
+                        }
+
+                    })
+                }
                 </div>  
 
 
